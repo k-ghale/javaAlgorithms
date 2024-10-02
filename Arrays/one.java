ba8972c23f41c0a0d31d19cf490c941826f98f4e
k-ghale <kabinghale888@gmail.com>
2024-10-01 23:16:20 -0400

    added one.java

---
+++ Arrays/one.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+
+//initiazing a fixed array
+public class One {
+  int[] arrayName = new int[5];
+  int[] arrayName = {10, 53, 65, 43};
+  int[] arrayName2 = new int[5];
+  int[] arrayName2 = {15,22,43,56};
+  System.out.println("Hello World");
+
+  for(int i = 0 ; i < 5 ; i++){
+    System.out.println(arrayName[i]);
+    System.out.println(arrayName2[i]);
+  }
+}
+
+
+
+
